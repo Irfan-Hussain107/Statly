01be780e81f09fb2f55e020edacf20c9d3629288
Irfan Hussain <irfanhussains98786@gmail.com>
2025-08-05 23:49:02 +0530

    refresh

--- api/index.js
+++ api/index.js
@@ -22,7 +22,7 @@ app.use(session({
 }));
 
 app.use(passport.initialize());
-app.use(passport.session());
+app.use(passport.session())
 
 app.use(cors({
     origin: [process.env.CLIENT_URL, 'http://localhost:5173'],
