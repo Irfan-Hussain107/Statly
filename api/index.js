b9ffaa59270128b9a7ddbfb13a5b3351fba53eef
Irfan Hussain <irfanhussains98786@gmail.com>
2025-08-06 01:02:36 +0530

    l

--- api/index.js
+++ api/index.js
@@ -32,7 +32,7 @@ app.use(cors({
 }));
 
 app.use(express.json({ limit: '10mb' }));
-app.use(cookieParser());
+app.use(cookieParser())
 
 app.use('/api/auth', require('./routes/authRoutes'));
 app.use('/api/platforms', require('./routes/platforms'));
